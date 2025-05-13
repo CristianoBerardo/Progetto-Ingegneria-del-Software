a8ae638db75d3e82941e27cae9c52f9973ea28ba
martinadep <martina.depiccoli@studenti.unitn.it>
2025-05-13 19:17:39 +0200

    init firebase signup

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,28 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCnCEYeCvKlVrd5nMLo43o6ioFugLF6zL0",
+  authDomain: "agritrento-ee49c.firebaseapp.com",
+  projectId: "agritrento-ee49c",
+  storageBucket: "agritrento-ee49c.firebasestorage.app",
+  messagingSenderId: "265629131019",
+  appId: "1:265629131019:web:d8cb065dedeaea109b420e",
+  measurementId: "G-ETEJWDTJJ6"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+
 const app = createApp(App)
 
 app.use(createPinia())
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,22 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+    { path: '/', name: 'home', component: () => import('../views/Home.vue'),},
+    { path: '/register', name: 'register', component: () => import('../views/Register.vue'),},
+    { path: '/sign-in', name: 'signin', component: () => import('../views/SignIn.vue'), },
+    { path: '/feed', name: 'feed', component: () => import('../views/Feed.vue'), },
   ],
 })
 
