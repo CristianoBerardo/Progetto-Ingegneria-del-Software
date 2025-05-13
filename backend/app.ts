a166886b92b41640f9862b8475d6feef92050779
Cristiano Berardo <cristianoberardo22@gmail.com>
2025-05-13 13:30:51 +0200

    cleanning projects

--- backend/app.ts
+++ backend/app.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { databaseProducts } from './connections/mongoDB/connections';
-import productRoutes from './routes/product.routes';
+// import productRoutes from './routes/product.routes';
 
 async function run() {
   try {
     await mongoose.connect(databaseProducts);
-      console.log("Pinged your deployment. You successfully connected to MongjdhfbhoDB!");
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
       const Model = mongoose.model('Pippo', new mongoose.Schema({ name: String }));
       await Model.createCollection();
   } finally {
@@ -19,6 +19,6 @@ run().catch(console.dir);
 const app = express();
 app.use(express.json());
 
-app.use('/api/products', productRoutes);
+// app.use('/api/products', productRoutes);
 
 export default app;
