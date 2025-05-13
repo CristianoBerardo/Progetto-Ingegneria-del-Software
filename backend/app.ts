feb3b47da842792ac366ea230239e38e309dbe2b
Cristiano Berardo <cristianoberardo22@gmail.com>
2025-05-13 16:20:40 +0200

    Rewrite connection to mongoDB

--- backend/app.ts
+++ backend/app.ts
@@ -1,30 +1,12 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import { databaseProducts } from './connections/mongoDB/connections';
-import productRoutes from './routes/ProductEndPoint';
+import { connectToMongoDB } from "./connections/mongoDB/connections";
 
-async function run() {
+export async function startServer() {
   try {
-    // await mongoose.connect(databaseProducts);
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-      // const Model = mongoose.model('Pippo', new mongoose.Schema({ name: String }));
-      // await Model.createCollection();
-  } finally {
-    // await mongoose.disconnect();
+    await connectToMongoDB();
+  } catch (error) {
+    console.error("Error starting the server:", error);
+    throw new Error("Failed to start the server");
   }
-
 }
-run().catch(console.dir);
 
-const app = express();
-app.use(express.json());
-
-// Use the product routes
-app.use('/api/products', productRoutes);
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
-
-export default app;
+startServer()
