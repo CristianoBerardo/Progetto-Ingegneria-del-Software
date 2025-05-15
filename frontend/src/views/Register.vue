f879473d91fa6716c52c7b0d66e4b706af18003a
martinadep <martina.depiccoli@studenti.unitn.it>
2025-05-15 12:17:06 +0200

    added sign in and sign out

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -2,6 +2,7 @@
     <h1>Create an Account</h1>
     <p><input type="text" placeholder="Email" v-model="email"/></p>
     <p><input type="password" placeholder="Password" v-model="password"/></p>
+    <p v-if="errMsg"> {{ errMsg }}</p>
     <p><button @click="register">Submit</button></p>
     <p><button @click="signInWithGoogle">Sign In With Google</button></p>
 
@@ -14,18 +15,33 @@ import { useRouter } from 'vue-router';
 const email = ref("");
 const password = ref("");
 const router = useRouter();
+const errMsg = ref();
+
 const register = async () => {
-    createUserWithEmailAndPassword(getAuth(), email.value, password.value)
-        .then((userCredential) => {
+    const auth = getAuth(); //saved in local storage by default
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+        .then((data) => {
             // Signed in
-            const user = userCredential.user;
-            console.log(user);
+            console.log("Successfully registered!");
+            console.log(auth.currentUser);
             router.push('/feed');
         })
         .catch((error) => {
             const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode, errorMessage);
+            switch (errorCode) {
+                case 'auth/invalid-email':
+                    errMsg.value = "Invalid email";
+                    break;
+                case 'auth/email-already-in-use':
+                    errMsg.value = "This email is already registered\nWant to sign in?";
+                    break;
+                case 'auth/weak-password':
+                    errMsg.value = "Password should be at least 6 characters";
+                    break;
+                default:
+                    errMsg.value = "Email or password was incorrect";
+                    break;
+            }
         });
 };
 const signInWithGoogle = async () => {
--- frontend/src/views/SignIn.vue
+++ frontend/src/views/SignIn.vue
@@ -1,5 +1,58 @@
 <template>
-    <div class="sign-in">
-        <h1>Sign In</h1>
-    </div>
-  </template>
+    <h1>Create an Account</h1>
+    <p><input type="text" placeholder="Email" v-model="email"/></p>
+    <p><input type="password" placeholder="Password" v-model="password"/></p>
+    <p v-if="errMsg"> {{ errMsg }}</p>
+    <p><button @click="signIn">Submit</button></p>
+    <p><button @click="signInWithGoogle">Sign In With Google</button></p>
+
+</template>
+<script setup>
+import { ref } from 'vue';
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+import { useRouter } from 'vue-router';
+
+const email = ref("");
+const password = ref("");
+const errMsg = ref(); // error message
+const router = useRouter();
+
+
+const signIn = async () => {
+    const auth = getAuth(); //saved in local storage by default
+    signInWithEmailAndPassword(auth, email.value, password.value)
+        .then((data) => {
+            // Signed in
+            console.log("Successfully signed id!");
+            console.log(auth.currentUser);
+            router.push('/feed');
+        })
+        .catch((error) => {
+            console.log(error.code);
+            switch (error.code) {
+                case 'auth/invalid-email':
+                    errMsg.value = "Invalid email";
+                    break;
+                case 'auth/user-not-found':
+                    errMsg.value = "This email is not registered";
+                    break;
+                case 'auth/wrong-password':
+                    errMsg.value = "Incorrect password";
+                    break;
+                default:
+                    errMsg.value = "Email or password was incorrect";
+                    break;
+            }
+        });
+};
+const signInWithGoogle = async () => {
+    const response = await fetch('http://localhost:3000/api/auth/google', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const data = await response.json();
+    console.log(data);
+};
+</script>
