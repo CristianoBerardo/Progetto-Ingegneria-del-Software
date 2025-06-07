da1b3b3205bacf77c3254222b5750f1fab4d557d
Cristiano Berardo <75799096+CristianoBerardo@users.noreply.github.com>
2025-06-07 12:40:55 +0200

    End products test

---
+++ backend/__tests__/costants/loginUsers.ts
@@ -0,0 +1,14 @@
+export const TEST_USERS = {
+  client: {
+    email: "cri@gmail.com",
+    password: "cristiano123",
+  },
+  producer: {
+    email: "az1@gmail.com",
+    password: "az1@gmail.com",
+  },
+  admin: {
+    email: "admin@admin.it",
+    password: "admin@admin.it",
+  },
+};
